60409ba3645ae295c8a3d58e17e79703ad291294
Duncan Leggat <ccaedle@ucl.ac.uk>
2025-05-15 14:38:27 +0100

    Hardware boundary bits

--- interface/forms.py
+++ interface/forms.py
@@ -11,4 +11,16 @@ class OperationalImpactValues(forms.Form):
     energy_consumption = forms.FloatField(label="Energy consumption (in kWh)")
     carbon_footprint = forms.FloatField(label="Carbon footprint (in gCO2e)")
     water_consumption = forms.FloatField(label="Water consumption (in litres)")
-
+    hardware_boundary_choices = (
+        ("1", "CPUs"),
+        ("2", "GPUs"),
+        ("3", "Memory"),
+        ("4", "Storage"),
+        ("4", "Networking"),
+    )
+    hardware_boundaries = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        choices=hardware_boundary_choices
+    )
+    details_of_hardware = forms.CharField(label="Hardware description", max_length=5000)
+    
