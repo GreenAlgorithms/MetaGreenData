029a31b144a39e1b1bf2a30a94d80ebe9dd7259c
haessar <wahhill@gmail.com>
2025-05-28 16:28:48 +0100

    make use of app inheritance for urls

--- interface/urls.py
+++ interface/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('metadata/', views.metadata_form_view, name='metadata_form'),
-    path('metadata/yml-preview/', views.get_yml_preview, name='yml_preview'),
-    path('metadata/download/', views.download_yml, name='download_yml'),
+    path('', views.computing_form_view, name='computing'),
+    path('yml-preview/', views.get_yml_preview, name='yml_preview'),
+    path('download/', views.download_yml, name='download_yml'),
 ]
