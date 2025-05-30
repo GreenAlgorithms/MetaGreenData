64abaeb873142ff84e2f12b3a9b411b61f290f91
sakshikumar19 <sakshinumberone@gmail.com>
2025-05-30 18:57:13 +0530

    Improved form validation feedback

--- interface/forms.py
+++ interface/forms.py
@@ -29,15 +29,15 @@ class OperationalImpactForm(forms.Form):
     # Impact values
     energy_consumption = forms.FloatField(
         label="Energy consumption (in kWh)",
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number'})
     )
     carbon_footprint = forms.FloatField(
         label="Carbon footprint (in gCO2e)",
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number'})
     )
     water_consumption = forms.FloatField(
         label="Water consumption (in liters)",
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number'})
     )
 
     # Methods and scope
@@ -75,7 +75,7 @@ class OperationalImpactForm(forms.Form):
     
     pue_value = forms.FloatField(
         label="Power Usage Effectiveness (PUE) Value",
-        widget=forms.NumberInput(attrs={'class': 'form-control', 'min': '1'}),
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number ≥ 1', 'min': '1'}),
         required=False
     )
     
@@ -87,7 +87,7 @@ class OperationalImpactForm(forms.Form):
     
     wue_value = forms.FloatField(
         label="Water Usage Effectiveness (WUE) Value (in L/kWh)",
-        widget=forms.NumberInput(attrs={'class': 'form-control'}),
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number'}),
         required=False
     )
     
@@ -99,7 +99,7 @@ class OperationalImpactForm(forms.Form):
     
     electrical_carbon_intensity = forms.FloatField(
         label="Electrical Carbon Intensity Value (in gCO2e/kWh)",
-        widget=forms.NumberInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number'})
     )
     
     electrical_carbon_intensity_source = forms.CharField(
@@ -112,22 +112,22 @@ class EmbodiedImpactForm(forms.ModelForm):
         model = EmbodiedImpact
         fields = '__all__'
         widgets = {
-            'carbon_footprint': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'gCO2e'}),
+            'carbon_footprint': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (gCO2e)'}),
             'carbon_footprint_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
-            'depletion_abiotic': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'kg Sb-eq'}),
+            'depletion_abiotic': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (kg Sb-eq)'}),
             'depletion_abiotic_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
-            'particulate_matter': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'disease incidence'}),
+            'particulate_matter': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (disease incidence)'}),
             'particulate_matter_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
-            'acidification_potential': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'kg mol H+'}),
+            'acidification_potential': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (kg mol H+)'}),
             'acidification_potential_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
-            'ionising_radiation': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'kBq U-235'}),
+            'ionising_radiation': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (kBq U-235)'}),
             'ionising_radiation_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
-            'photochemical_ozone': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'kg NMVOC-eq'}),
+            'photochemical_ozone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (kg NMVOC-eq)'}),
             'photochemical_ozone_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
-            'abiotic_depletion_fossil': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'MJ'}),
+            'abiotic_depletion_fossil': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (MJ)'}),
             'abiotic_depletion_fossil_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
-            'freshwater_ecotoxicity': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'CTUe'}),
-            'freshwater_ecotoxicity_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'}),
+            'freshwater_ecotoxicity': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter a number (CTUe)'}),
+            'freshwater_ecotoxicity_source': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Source and method'})
         }
 
 class BasicInformationForm(forms.ModelForm):
